2179fee443465a711a046a061fe179f7b702246e
will <macbook@Piotrs-MacBook-Pro.local>
2018-09-24 19:47:16 +0200

    initial commit dotNetCore

---
+++ dotnetCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
