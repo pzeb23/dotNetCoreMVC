016c6378e3f6ab041c5f1d77ff3c1f1306773f73
pzeb23 <pzeb23@gmail.com>
2018-09-29 10:18:23 +0200

    Send e-mail button

---
+++ dotnetCoreMVC/Controllers/SendMailController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetCoreMVC.Controllers
+{
+    public class SendMailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("mail sent!");
+        }
+    }
+}
