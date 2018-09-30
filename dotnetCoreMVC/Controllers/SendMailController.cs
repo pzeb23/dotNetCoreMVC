035256661075ac31a544b586f0e65d4019b3c008
MacbookVisual <spotify002@o2.pl>
2018-09-30 17:42:53 +0200

    Send method moved to EmailService

--- dotnetCoreMVC/Controllers/SendMailController.cs
+++ dotnetCoreMVC/Controllers/SendMailController.cs
@@ -1,10 +1,4 @@
-﻿using System.Linq;
-using dotnetCoreMVC.Models;
-using MailKit.Net.Smtp;
-using Microsoft.AspNetCore.Mvc;
-using MimeKit;
-using MimeKit.Text;
-using dotnetCoreMVC.Interfaces;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace dotnetCoreMVC.Controllers
 {
@@ -13,39 +7,6 @@ namespace dotnetCoreMVC.Controllers
         public IActionResult SendMail()
         {
             return View();
-        }
-
-        public void Send(EmailMessageModel emailMessage, IEmailConfiguration emailConfiguration)
-        {
-            var _emailConfiguration = emailConfiguration;
-
-            var message = new MimeMessage();
-            message.To.AddRange(emailMessage.ToAddresses.Select(x => new MailboxAddress(x.Name, x.Address)));
-            message.From.AddRange(emailMessage.FromAddresses.Select(x => new MailboxAddress(x.Name, x.Address)));
-
-            message.Subject = emailMessage.Subject;
-            //We will say we are sending HTML. But there are options for plaintext etc. 
-            message.Body = new TextPart(TextFormat.Html)
-            {
-                Text = emailMessage.Content
-            };
-
-            //Be careful that the SmtpClient class is the one from Mailkit not the framework!
-            using (var emailClient = new SmtpClient())
-            {
-                //The last parameter here is to use SSL (Which you should!)
-                emailClient.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.SmtpPort, true);
-
-                //Remove any OAuth functionality as we won't be using it. 
-                emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
-
-                emailClient.Authenticate(_emailConfiguration.SmtpUsername, _emailConfiguration.SmtpPassword);
-
-                emailClient.Send(message);
-
-                emailClient.Disconnect(true);
-            }
-
         }
     }
 }
--- dotnetCoreMVC/Services/EmailService.cs
+++ dotnetCoreMVC/Services/EmailService.cs
@@ -2,12 +2,15 @@
 using dotnetCoreMVC.Models;
 using System;
 using System.Collections.Generic;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
 
 namespace dotnetCoreMVC.Services
 {
     public class EmailService : IEmailService
     {
-        private readonly IEmailConfiguration _emailConfiguration;
+        readonly IEmailConfiguration _emailConfiguration;
 
         public EmailService(IEmailConfiguration emailConfiguration)
         {
@@ -21,7 +24,32 @@ namespace dotnetCoreMVC.Services
 
         public void Send(EmailMessageModel emailMessage)
         {
-            throw new NotImplementedException();
+            var message = new MimeMessage();
+            message.To.AddRange(emailMessage.ToAddresses.Select(x => new MailboxAddress(x.Name, x.Address)));
+            message.From.AddRange(emailMessage.FromAddresses.Select(x => new MailboxAddress(x.Name, x.Address)));
+
+            message.Subject = emailMessage.Subject;
+            //We will say we are sending HTML. But there are options for plaintext etc. 
+            message.Body = new TextPart(TextFormat.Html)
+            {
+                Text = emailMessage.Content
+            };
+
+            //Be careful that the SmtpClient class is the one from Mailkit not the framework!
+            using (var emailClient = new SmtpClient())
+            {
+                //The last parameter here is to use SSL (Which you should!)
+                emailClient.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.SmtpPort, true);
+
+                //Remove any OAuth functionality as we won't be using it. 
+                emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                emailClient.Authenticate(_emailConfiguration.SmtpUsername, _emailConfiguration.SmtpPassword);
+
+                emailClient.Send(message);
+
+                emailClient.Disconnect(true);
+            }
         }
     }
 }
