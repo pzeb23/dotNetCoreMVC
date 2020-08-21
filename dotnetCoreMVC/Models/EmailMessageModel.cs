e3a1e6dc7922e719a6550e03af4767f050d42dac
pzeb23 <37831941+pzeb23@users.noreply.github.com>
2020-08-21 13:57:45 +0200

    Add files via upload

---
+++ dotnetCoreMVC/Models/EmailMessageModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Portfolio3.Models
+{
+    public class EmailMessageModel
+    {
+        public EmailMessageModel()
+        {
+            ToAddresses = new List<EmailAddressModel>();
+            FromAddresses = new List<EmailAddressModel>();
+        }
+
+        public List<EmailAddressModel> ToAddresses { get; set; }
+        public List<EmailAddressModel> FromAddresses { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
